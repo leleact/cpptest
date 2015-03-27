17de809e2f9ab569aeacfeef2a656b2f9228ba3a
leleact <leleact@gmail.com>
2015-03-27 21:44:39 +0800

    emptystring

--- emptystring/emptystring.cpp
+++ emptystring/emptystring.cpp
@@ -4,6 +4,7 @@ int main() {
 	std::string s;
 	std::cout << "s = " << s << std::endl;
 	std::cout << "s.size = " << s.size() << std::endl;
+	std::cout << "s.capacity = " << s.capacity() << std::endl;
 	std::cout << "s[0] = " << s[0] << std::endl;
 	return 0;
 }
