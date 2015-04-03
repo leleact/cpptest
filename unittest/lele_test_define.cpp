ce1f626bc183822c5f39405fc2d102aac4688a69
leleact <leleact@gmail.com>
2015-04-03 21:21:39 +0800

    define

--- unittest/lele_test_define.cpp
+++ unittest/lele_test_define.cpp
@@ -2,6 +2,9 @@
 #define LINT int
 #define LCHAR char
 int main(LINT argc, LCHAR **argv) {
+	for (int i = 0; i < argc; i++) {
+		std::cout << "第[" << i + 1 << "]个参数是[" << *(argv + i) << "]" << std::endl;
+	}
 	std::cout << "Hello World!" << std::endl;
 	return 0;
 }
