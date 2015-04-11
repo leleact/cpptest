014b3f15b83b102f775f55a033baffff7cf8ff45
leleact <leleact@gmail.com>
2015-04-11 23:43:55 +0800

    endian

--- endian/lele_test_endian.c
+++ endian/lele_test_endian.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	int a = 1;
+	int a = 0x01020304;
 	char *ptr = (char *)(&a);
 	for (size_t i = 0; i < sizeof(a); i++) {
-		printf("[0x%02x]\n", *(ptr + i));
+		printf("addr = [0x%lx], value = [0x%02x]\n", (unsigned long)(ptr + i), *(ptr + i));
 	}
 }
