d0aaeddb51f018aabb32c7530b59e0e999d93057
leleact <leleact@gmail.com>
2015-04-26 22:46:06 +0800

    algorithm

---
+++ algorithm/lele_test_algorithm.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstring>
+#include <time.h>
+#include <sys/time.h>
+using namespace std;
+
+void bubble_sort(int *, int);
+void printf_array(int *, int);
+void swap(int *, int *);
+void writetofile(int *, int, char *);
+
+#define MAXLEN 10000
+#define CYCTIMES 1
+
+int main(int argc, char **argv) {
+	if (argc != 3) {
+		std::cout << "Please use: main <outFileName1> <outFileName2>" << std::endl;	
+		return 1;
+	}
+	char outFileName1[256];
+	char outFileName2[256];
+	memset(outFileName1, 0x00, sizeof(outFileName1));
+	memset(outFileName2, 0x00, sizeof(outFileName2));
+	strcpy(outFileName1, argv[1]);
+	strcpy(outFileName2, argv[2]);
+	struct timeval start, end;
+	int Testarray[MAXLEN];
+	for (int i = 0; i < MAXLEN; i++)
+		Testarray[i] = i;
+
+	srand(time(NULL));
+	for (int i = MAXLEN - 1; i > 0;  --i) {
+		int index = rand() % i;	
+		swap(Testarray + i, Testarray + index);
+	}
+
+	writetofile(Testarray, MAXLEN, outFileName1);
+	gettimeofday(&start, NULL);
+	for (int i = 0; i < CYCTIMES; ++i) {
+		bubble_sort(Testarray, MAXLEN);
+	}
+	gettimeofday(&end, NULL);
+	writetofile(Testarray, MAXLEN, outFileName2);
+	printf("%.6lf\n", end.tv_sec - start.tv_sec + (double)(end.tv_usec - start.tv_usec)/1000000);
+}
+
+void bubble_sort(int *array, int n) {
+//	printf_array(array, 10);
+	for(int i = 0; i < n; ++i) {
+		for (int j = 0; j < n - i - 1; ++j)	{
+			if (array[j] > array[j + 1]) {
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+//	printf_array(array, 10);
+}
+
+void printf_array(int *array, int n) {
+	for (int i = 0; i < n; i++) {
+		printf("array[%d] = [%d]\n", i, array[i]);
+	}
+}
+
+void swap(int *p, int *q) {
+	int temp = *p;
+	*p = *q;
+	*q = temp;
+}
+
+void writetofile(int *array, int n, char *str) {
+	fstream file;
+	file.open(str, fstream::out | fstream::trunc);
+	for (int i = 0; i < n; ++i) {
+		file << setw(4) << array[i] << " ";
+		if ( (i + 1) % 16 == 0) file << "\n";
+	}
+
+	file.close();
+}
