9d10650f9424671748e193d2a4cf0022efae8b9f
leleact <leleact@gmail.com>
2015-05-14 00:25:02 +0800

    fflush

--- fflush/lele_test_fflush.c
+++ fflush/lele_test_fflush.c
@@ -5,7 +5,7 @@ int main() {
 	int iCount = 0;
 	while(iCount++ < 10) {
 //		printf("i = [%d]\n", ++i); // 如果printf后面有'\n'会自动刷新缓冲区
-		printf("i = [%d]", ++i); // 不加fflush时，直到程序结束才回刷新缓冲区
+		printf("i = [%d]", ++i);   // 不加fflush时，直到程序结束才回刷新缓冲区
 		sleep(5);
 		fflush(stdout);
 	}
