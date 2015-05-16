76878f9f3b544ebfe1d7a87473304e27b1f072a1
leleact <leleact@gmail.com>
2015-05-16 21:05:02 +0800

    move

--- c++/move/lele_test_move.cpp
+++ c++/move/lele_test_move.cpp
@@ -16,9 +16,23 @@ public:
 #endif
     s = std::move(string(p)); 
   }
-  X(string &&s) : s(std::move(s)) {}
+  X(string &&s) : s(std::move(s)) {
+#ifdef DEBUG
+    std::cout << "string && move" << std::endl;
+#endif
+  }
+  X(const X &x) : s(x.s) {}
+  X(X&& x) : s(std::move(x.s)) {
+#ifdef DEBUG
+    std::cout << "X&& move" << std::endl;
+#endif
+  }
   X& operator=(const X &x) {
     s = x.s; 
+    return *this;
+  }
+  X& operator=(X&& x) {
+    s = std::move(x.s);
     return *this;
   }
 
@@ -33,16 +47,23 @@ private:
 using X = std::string;
 #endif
 int main() {
+  std::string s1 = "aa";
+  std::string s2 = "bb";
   X x1 = "aa";
   X x2 = "bb";
+  X x3(s1);
+  X x4(s2);
   std::vector<X> xvec;
   xvec.push_back(x1);
   xvec.push_back(std::move(x2));
+  xvec.push_back(x3);
+  xvec.push_back(std::move(x4));
 
   for (X& x : xvec) std::cout << x << " ";
   std::cout << std::endl;
 
   std::cout << "x1 = [" << x1 << "], x2 = [" << x2 << "]" << std::endl;
+  std::cout << "x3 = [" << x3 << "], x4 = [" << x4 << "]" << std::endl;
 
   return 0;
 }
