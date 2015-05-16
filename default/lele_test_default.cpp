ca297683a90d9a8b5bbf32216d9079689430487c
leleact <leleact@gmail.com>
2015-05-16 15:21:39 +0800

    default

--- default/lele_test_default.cpp
+++ default/lele_test_default.cpp
@@ -13,6 +13,7 @@ private:
 };
 
 int main(int argc, char **argv) {
+//X x1; = default 不能直接用来构造对象, 但是当需要用到默认构造函数时, 居然可以用到 eg:make_shared
   X x(3);
   std::shared_ptr<X> ptr = std::make_shared<X>();
   std::cout << ptr->Show() << std::endl;
