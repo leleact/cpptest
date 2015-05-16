f9a3f2da8605b31d4724561be7c7840d03cf62ee
leleact <leleact@gmail.com>
2015-05-16 16:36:36 +0800

    func

--- c++/func/lele_test_func.cpp
+++ c++/func/lele_test_func.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+/*
 void func(string &s) {
  cout << "string &" << endl;
 }
+*/
 
 void func(string &&s) {
  cout << "string &&" << endl;
@@ -13,9 +15,28 @@ void func(const string &s) {
  cout << "const string &" << endl;
 }
 
+
 void func(const string &&s) {
  cout << "const string &&" << endl;
 }
+
+void func1(const string &s, int i = 100) {
+ cout << "const string &, int" << endl;
+}
+
+void func1(const string &s, bool i = true) {
+ cout << "const string &, bool" << endl;
+}
+
+void func1(const string &s, string s1) {
+ cout << "const string &, string" << endl;
+}
+
+/*
+void func1(const string &s, char i = 'a') {
+ cout << "const string &, char" << endl;
+}
+*/
 
 int main() {
   string s;
@@ -23,5 +44,6 @@ int main() {
   const string s1;
   func(s1);
   func("a");
+  func1(s, 'a');
   return 0;
 }
