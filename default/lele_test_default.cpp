dd846a87e24bdd73c9daf7b03261067dce188526
leleact <leleact@gmail.com>
2015-05-16 15:19:16 +0800

    default

--- default/lele_test_default.cpp
+++ default/lele_test_default.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <memory>
 class X {
 public:
   X() = default;
-  //X(int a = 0) : i(a) {}
+  X(int a) : i(a) {}
+  int  Show() {
+    std::cout << i << std::endl; 
+    return i;
+  }
 private:
   int i;
 };
+
 int main(int argc, char **argv) {
-  X x; 
-//  X x();
-  //X x1(4);
+  X x(3);
+  std::shared_ptr<X> ptr = std::make_shared<X>();
+  std::cout << ptr->Show() << std::endl;
   return 0;
 }
