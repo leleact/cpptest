09f610723cf33f2a5643163a9fe0f85ad98506ea
leleact <leleact@gmail.com>
2015-05-27 22:24:03 +0800

    vector

--- c++/vector/lele_test_vector.cpp
+++ c++/vector/lele_test_vector.cpp
@@ -10,11 +10,32 @@ int main() {
   }
 
   std::vector<int>::iterator iter = vec.begin();
+  /*
   for (; iter != vec.end();) {
-    std::cout << *iter << std::endl;
-    vec.insert(iter, 40);
-    std::cout << *iter << std::endl;
-    ++iter;
+    if (*iter == 6) {
+      std::cout << *iter << std::endl; 
+      std::vector<int>::iterator iter1 = vec.erase(iter);
+      std::cout << *iter << std::endl; 
+      std::cout << *iter1 << std::endl; 
+    }
+    else
+      iter++;
   }
+  */
+
+  /* 死循环 */
+  for (; iter != vec.end(); ) {
+    std::cout << *iter << " ";
+    vec.insert(iter, 40);
+    std::cout << *iter << " ";
+    iter++;
+    std::cout << *iter << " ";
+    ++iter;
+    std::cout << *iter << std::endl;
+  }
+
+  for (iter = vec.begin(); iter != vec.end(); ++iter)
+    std::cout << *iter << " ";
+  std::cout << std::endl;
 }
 
