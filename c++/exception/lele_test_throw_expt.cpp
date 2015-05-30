19a8a3907abbce94daf52b44ed73f01beff83123
leleact <leleact@gmail.com>
2015-05-30 13:36:26 +0800

    exception

--- c++/exception/lele_test_throw_expt.cpp
+++ c++/exception/lele_test_throw_expt.cpp
@@ -17,6 +17,8 @@ public:
 };
 
 void func() throw (std::string) ;
+void func1() noexcept;
+void func2() noexcept(0);
 
 int main() {
   std::string str = "aaa";
@@ -39,13 +41,39 @@ catch (int n) {
   }
   */
 
+//try {
+//  func1();
+// } catch (int n) {
+//  std::cout << "func1 int exception" << std::endl;
+//}
+
+
+try {
+  func2();
+} catch (int n) {
+  std::cout << "func2 int exception" << std::endl;
+}
+
   return 0;
 }
 
-
+/**
+ * C++11 弃用throw()这种用法, 改用noexcept修饰符号
+ */
 void func() throw (std::string) {
   std::cout << "func called" << std::endl;
   if (1)
     // throw int(); // 直接退出, 不能捕获
     throw std::string("hehe"); // 可以被捕获与处理
+}
+
+void func1() noexcept {
+  std::cout << "func1 called" << std::endl;
+  throw 1; // 因为noexcpt 无法被捕获
+}
+
+// void func2 {
+void func2() noexcept(0) {
+  std::cout << "func2 called" << std::endl;
+  throw 1; // noexcpt(0) 相当于没有noexcpt 异常可以被捕获
 }
