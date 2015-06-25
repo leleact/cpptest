c6e0c42bd102243413e89c6372725e27758d0b2b
lelealien <leleact@gmail.com>
2015-06-26 00:30:28 +0800

    iconv

--- gbktoutf8/GbkToUtf8.cpp
+++ gbktoutf8/GbkToUtf8.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <iconv.h>
+#include <errno.h>
 int GbkToUtf8(char *lpSrc, size_t szInlen, char *lpDes, size_t szOutlen)
 {
 	if (lpSrc == NULL || lpDes == NULL || szInlen <= 0 || szOutlen <= 0)
@@ -18,8 +19,13 @@ int GbkToUtf8(char *lpSrc, size_t szInlen, char *lpDes, size_t szOutlen)
 	}
 	
 	memset(lpDes, 0x00, szOutlen);
-    if (iconv(cd, pIn, &szInlen, pOut, &szOutlen) == -1)
+	std::size_t nRet = 0;
+    nRet = iconv(cd, pIn, &szInlen, pOut, &szOutlen) ;
+	std::cout << "nRet = [" << nRet << "]" << std::endl;
+	std::cout << "errno = [" << errno << "], ErrMsg = [" << strerror(errno) << "]" << std::endl;
+	if (nRet == -1)
 	{
+		std::cout << "nRet 能等于 -1？" << std::endl;
 		iconv_close(cd);
         return -1;
 	}
@@ -31,9 +37,9 @@ int GbkToUtf8(char *lpSrc, size_t szInlen, char *lpDes, size_t szOutlen)
 int main()
 {
 	char src[256] = "Hello, 你好";
-	char des[256 + 128] = { 0 };
-	size_t nLenIn = 256;
-	size_t nLenOut = 256 + 128;
+	char des[256] = { 0 };
+	std::size_t nLenIn = 256;
+	std::size_t nLenOut = 256;
 	int nRet = 0;
 	nRet = GbkToUtf8(src, nLenIn, des, nLenOut);
 	if (nRet)
