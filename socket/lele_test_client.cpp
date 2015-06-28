cbea43c0b57f67723954c78ca6794d11cdc99861
lelealien <leleact@gmail.com>
2015-06-28 11:18:52 +0800

    status

--- socket/lele_test_client.cpp
+++ socket/lele_test_client.cpp
@@ -12,63 +12,63 @@
 #include <arpa/inet.h> /* inet_pton */
 
 int main(int argc, char **argv) {
-  /**
-   * argv[0] 程序名称
-   * argv[1] 发往IP地址
-   * argv[2] 发往端口
-   * argv[3] 发送文件名称
-   */
-  if (4 != argc) {
-    std::cout << "please using <client> <IPaddr> <port> <filename>" << std::endl;
-    return -1;
-  }
+    /**
+     * argv[0] 程序名称
+     * argv[1] 发往IP地址
+     * argv[2] 发往端口
+     * argv[3] 发送文件名称
+     */
+    if (4 != argc) {
+        std::cout << "please using <client> <IPaddr> <port> <filename>" << std::endl;
+        return -1;
+    }
 
-  std::string strIPAddr = argv[1];
-  unsigned short usPort = atoi(argv[2]);
-  std::string strFileName = argv[3];
+    std::string strIPAddr = argv[1];
+    unsigned short usPort = atoi(argv[2]);
+    std::string strFileName = argv[3];
 
-  int nFd = open(strFileName.c_str(), O_RDONLY);
-  if (nFd <= 0) {
-    std::cerr << "open error" << std::endl;
-    return -1;
-  }
+    int nFd = open(strFileName.c_str(), O_RDONLY);
+    if (nFd <= 0) {
+        std::cerr << "open error" << std::endl;
+        return -1;
+    }
 
-  // 准备socket
-  int nSocket = socket(AF_INET, SOCK_STREAM, 0);
-  if (-1 == nSocket) {
-    std::cerr << "socket() error" << std::endl;
-    return -1;
-  }
+    // 准备socket
+    int nSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (-1 == nSocket) {
+        std::cerr << "socket() error" << std::endl;
+        return -1;
+    }
 
-  // 填充IP地址和端口
-  struct sockaddr_in stSockAddr;
-  memset(&stSockAddr, 0x00, sizeof(stSockAddr));
-  stSockAddr.sin_family = AF_INET;
-  stSockAddr.sin_port = htons(usPort);
-  int nRet = 0;
-  nRet = inet_pton(AF_INET, strIPAddr.c_str(), &stSockAddr.sin_addr);
-  if (nRet == -1) {
-    std::cerr << "inet_pton() error" << std::endl;
-    return -1;
-  } 
-  
-  // 建立连接
-  nRet = connect(nSocket, (sockaddr *)&stSockAddr, sizeof(stSockAddr));
-  if (-1 == nRet) {
-    std::cerr << "connect error" << std::endl;
-    return -1;
-  }
+    // 填充IP地址和端口
+    struct sockaddr_in stSockAddr;
+    memset(&stSockAddr, 0x00, sizeof(stSockAddr));
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(usPort);
+    int nRet = 0;
+    nRet = inet_pton(AF_INET, strIPAddr.c_str(), &stSockAddr.sin_addr);
+    if (nRet == -1) {
+        std::cerr << "inet_pton() error" << std::endl;
+        return -1;
+    }
 
-  const int nMaxBuffSize = 4096;
-  char czBuff[nMaxBuffSize] = {0};
-  std::size_t nRead = 0;
-  while((nRead = read(nFd, czBuff, sizeof(czBuff))) != 0) {
-    sleep(3);
-    write(nSocket, czBuff, nRead); 
-  }
+    // 建立连接
+    nRet = connect(nSocket, (sockaddr *)&stSockAddr, sizeof(stSockAddr));
+    if (-1 == nRet) {
+        std::cerr << "connect error" << std::endl;
+        return -1;
+    }
 
-  close(nSocket);
-  close(nFd);
+    const int nMaxBuffSize = 4096;
+    char czBuff[nMaxBuffSize] = {0};
+    std::size_t nRead = 0;
+    while((nRead = read(nFd, czBuff, sizeof(czBuff))) != 0) {
+        sleep(3);
+        write(nSocket, czBuff, nRead);
+    }
 
-  return 0;
+    close(nSocket);
+    close(nFd);
+
+    return 0;
 }
