fc5015c10ec49810ca8bbee47e7d6482a48469f1
lelealien <leleact@gmail.com>
2016-01-15 16:54:27 +0800

    epoll

--- epoll/lele_test_server.cpp
+++ epoll/lele_test_server.cpp
@@ -18,8 +18,7 @@ void setnonblocking(int fd);
 char *getSocketAddr(int fd, char *socketAddr) ;
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
+	if (argc < 2) {
 		std::cerr << "please use <server> <port>!" << std::endl;	
 		return -1;
 	}
--- epoll/socketException.hpp
+++ epoll/socketException.hpp
@@ -1,6 +1,9 @@
 #ifndef _VARLIB_SOCKET_EXCEPTION_H
 #define _VARLIB_SOCKET_EXCEPTION_H
+#include <iostream>
 #include <exception>
+#include <string>
+#include <sstream>
 #include <errno.h>
 #include <string.h>
 
@@ -10,13 +13,15 @@ class socketException : public std::exception {
 public:
 	socketException(int error) :m_errno(error) { }
 
-	virtual const char *what() const noexcept {
-		return strerror(m_errno);
+	virtual const char *what() noexcept {
+		std::ostringstream os;
+		os << "errno: " << m_errno << ", " << strerror(m_errno);	
+		m_errmsg = os.str();
+		return m_errmsg.c_str();
 	}
 private:
 	int m_errno;
+	std::string m_errmsg;
 };
-
 }
-
 #endif
