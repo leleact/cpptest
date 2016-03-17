1db66916fc2e56ed4768c8ea1b55754a355ad8b2
lelealien <leleact@gmail.com>
2016-03-17 22:56:08 +0800

    UnitTest

--- UnitTest/src/TestCase.h
+++ UnitTest/src/TestCase.h
@@ -8,20 +8,23 @@
 #define TestCase(name)                                                     \
     class TestCase_##name : public TestProc {                              \
     public:                                                                \
-        TestCase_##name( std::list<std::shared_ptr<TestProc>>& list ) {    \
-                      list.push_back(std::shared_ptr<TestProc>(this));     \
-                  }                                                        \
-        virtual ~TestCase_##name() {}                                      \
+        TestCase_##name() {}                                               \
+        virtual ~TestCase_##name() { std::cout << "~" << std::endl;}       \
         virtual void run() const override;                                 \
-    } TestCase_##name##_Instance( UnitTest::getList() );                   \
+    } TestCase_##name##_Instance;                                          \
     void TestCase_##name::run() const
 
-class TestProc {
+
+
+
+class TestProc : public std::enable_shared_from_this<TestProc> {
     public:
         virtual ~TestProc() {}
         virtual void run() const {
             std::cout << "[" << __FILE__ << ":" << __LINE__ << "](" << __func__ << ")" << std::endl;
         }
 };
+
+
 
 #endif
