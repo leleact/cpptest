346b4ed54ae2e9017e7499236d77a595256beac9
lelealien <leleact@gmail.com>
2016-07-24 20:43:19 +0800

    base64

--- base64/src/Main.cpp
+++ base64/src/Main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <cstring>
 #include "base64.h"
 int main(int argc, char *argv[]) {
-    std::string str = "我是中国人a";
-    std::cout << base64_encode(reinterpret_cast<const unsigned char *>(str.c_str()), static_cast<int>(str.size())) << std::endl;
+    if (argc != 3) {
+        std::cerr << "Argument error!"  << std::endl;
+        return -1;
+    }
+
+    char *type = argv[1];
+    char *in = argv[2];
+    std::string str = in;
+    if (strncmp("encode", type, 5) == 0) {
+        std::cout << base64_encode(reinterpret_cast<const unsigned char *>(str.c_str()), static_cast<int>(str.size())) << std::endl;
+    } else if (strncmp("decode", type, 5) == 0) {
+        std::cout << base64_decode(str) << std::endl; 
+    }
     return 0;
 }
