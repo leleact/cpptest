5d73e469c550058114c5084dd17720c0fb43657b
lelealien <leleact@gmail.com>
2017-02-14 10:58:53 +0800

    using

--- c++/using/lele_test_using.cpp
+++ c++/using/lele_test_using.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+
+#define PF std::cout << "(" << __FILE__ << ":" << __LINE__ << ")[" << __PRETTY_FUNCTION__ << "]" << std::endl
+
 namespace vacp {
+
 template<typename T1, typename T2 = int>
 struct Base {
-  Base() = default;
-  Base(const T1 &t1, const T2 &t2) : type1_value(t1), type2_value(t2) { std::cout << "lvalue constructor\n";}
-  Base(const T1 &&t1, const T2 &&t2) : type1_value(t1), type2_value(t2) {std::cout << "rvalue constructor\n";}
+  Base() { PF; }
+  Base(const T1 &t1, const T2 &t2) : type1_value(t1), type2_value(t2) { PF; }
+  Base(const T1 &&t1, const T2 &&t2) : type1_value(t1), type2_value(t2) { PF; }
   T1 type1_value;
   T2 type2_value;
   friend std::ostream& operator<<(std::ostream &os, Base &b) {
@@ -13,6 +17,7 @@ struct Base {
     return os;
   }
 };
+
 }
 
 template<typename T1, typename T2 = int>
