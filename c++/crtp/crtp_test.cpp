232d2760e3e64ed15706b7a3818efdf4b78dfb84
leleact <leleact@gmail.com>
2017-06-18 02:30:02 +0800

    crtp

--- c++/crtp/crtp_test.cpp
+++ c++/crtp/crtp_test.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 
 template <typename T>
-class Base {
-    public:
-        void func () {
-            static_cast<T *>(this)->func();
-        }
+class base {
+public:
+    void impl () {
+        static_cast<T *>(this)->impl();
+    }
 };
 
-class Derive1 : public Base<Derive1> {
-    public:
-        void func() const {
-            std::cout << __PRETTY_FUNCTION__ << std::endl;
-        }
+class derive1 : public base<derive1> {
+public:
+    void impl() const {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
 };
 
-class Derive2 : public Base<Derive2> {
-    public:
-        void func() const {
-            std::cout << __PRETTY_FUNCTION__ << std::endl;
-        }
+class derive2 : public base<derive2> {
+public:
+    void impl() const {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
 };
 
-int main(int argc, char *argv[]) {
-    Base<Derive1> *b1 = new Derive1(); 
-    Base<Derive2> *b2 = new Derive2(); 
+int main() {
+    base<derive1> *b1 = new derive1();
+    base<derive2> *b2 = new derive2();
 
-    b1->func();
-    b2->func();
+    b1->impl();
+    b2->impl();
 
     delete b1;
     delete b2;
