b60aa64242242b205687ff57406c94ef987ae244
leleact <leleact@gmail.com>
2017-07-20 23:14:39 +0800

    sh

--- git/command.sh
+++ git/command.sh
@@ -2,3 +2,7 @@
 # pull recursive modules
 git clone --recursive [URL] # clone with
 git submodule update --init --recursive # after clone
+
+
+git remote # show remote name
+git remote -v # show remote url
