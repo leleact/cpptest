9e6432df946bd5201e3e5407167b11c95dc8e29e
leleact <leleact@gmail.com>
2018-01-07 21:33:51 +0800

    template overload

--- c++/template/lele_test_template.cpp
+++ c++/template/lele_test_template.cpp
@@ -40,6 +40,8 @@ int increase<int>(int v1, int v2) {
 
 }
 
+namespace v1 {
+
 int main(int argc, char *argv[]) {
     {
         v2::Increaser v1;
@@ -48,9 +50,43 @@ int main(int argc, char *argv[]) {
     }
 
     {
-        int i1 = 1; 
+        int i1 = 1;
         int i2 = 2;
         std::cout << v2::increase(i1, i2) << std::endl;
     }
     return 0;
+}
+
+}
+
+namespace v0107 {
+    // 1
+    template<typename T>
+    void f(T&& v) {
+        std::cout << "(" << __FILE__ << ":" << __LINE__ << ")[" << __PRETTY_FUNCTION__ << "]" << std::endl;
+    }
+
+    // 2
+    template<typename T>
+    void f(const T& v) {
+        std::cout << "(" << __FILE__ << ":" << __LINE__ << ")[" << __PRETTY_FUNCTION__ << "]" << std::endl;
+    }
+
+    // 3
+    template<typename T>
+    void f(T& v) {
+        std::cout << "(" << __FILE__ << ":" << __LINE__ << ")[" << __PRETTY_FUNCTION__ << "]" << std::endl;
+    }
+
+    int main(int argc, char *argv[]) {
+        int i = 10;
+        f(10); // #1 存在的时候选1(T = int)， #1不存在时选择#2
+        f(i);  // #3
+        const int j = 20;
+        f(j);
+        return 0;
+    }
+}
+int main(int argc, char *argv[]) {
+    return v0107::main(argc, argv);
 }
