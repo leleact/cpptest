a37abadfe8a78e7a98e007a5cc76401e05d9d412
leleact <leleact@gmail.com>
2018-03-31 21:38:47 +0800

    add cmake

---
+++ tests/cmake_multiple_src_directory_test/CmakeMultipleSrcDirectoryTest.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(CmakeMultipleSrcDirectoryTest, CmakeMultipleSrcDirectoryTestName) {
+    std::cout << "CmakeMultipleSrcDirectoryTest" << std::endl;
+    EXPECT_EQ(1, 1);
+}
