5cb7a4f248232a5943e4e20cbfae3c2b60e0dd4f
leleact <leleact@gmail.com>
2018-04-15 18:59:25 +0800

    string_view constexpr test

---
+++ tests/string_view/string_view_constexpr.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+#include <iostream>
+#include <array>
+#include <string_view>
+
+template <typename ENV>
+struct Context {
+    constexpr static const std::string_view value = ENV::value;
+};
+
+struct DEV {
+    constexpr static const std::string_view value = "dev";
+};
+
+struct SIT {
+    constexpr static const std::string_view value = "sit";
+};
+
+struct UAT {
+    constexpr static const std::string_view value = "uat";
+};
+
+TEST(test_string_view, string_view_constexpr) {
+    EXPECT_EQ("uat", Context<UAT>::value);
+}
