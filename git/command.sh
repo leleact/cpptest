f50a9006e9e2ba0d1f182d1984d436cd918e1458
leleact <leleact@gmail.com>
2018-04-27 02:16:54 +0800

    remove git

--- git/command.sh
+++
@@ -1,8 +0,0 @@
-
-# pull recursive modules
-git clone --recursive [URL] # clone with
-git submodule update --init --recursive # after clone
-
-
-git remote # show remote name
-git remote -v # show remote url
