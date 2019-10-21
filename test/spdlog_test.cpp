461f94b92e3396f81a4781c3f09348e61345d285
leleact <leleact@gmail.com>
2019-10-21 17:18:25 +0800

    test: spdlog log level

--- test/spdlog_test.cpp
+++ test/spdlog_test.cpp
@@ -2,7 +2,15 @@
 #include "spdlog/spdlog.h"
 #include "gtest/gtest.h"
 
+#define LOGD(logger, fmt, ...)                                                 \
+  {                                                                            \
+    logger->debug("{}::{}()#{}: " fmt, __FILE__, __FUNCTION__, __LINE__,       \
+                  __VA_ARGS__);                                                \
+  }
+
 TEST(ExternalProject, spdlog) {
   auto console = spdlog::stderr_color_mt("console");
+  console->set_level(spdlog::level::trace);
   console->info("spdlog output, welcome to spdlog!");
+  LOGD(console, "{}", "xxx");
 }
