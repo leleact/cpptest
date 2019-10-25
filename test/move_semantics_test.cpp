544ba126d7ad4d50d02fa80d97ad8e25e5828110
leleact <leleact@gmail.com>
2019-10-25 20:49:24 +0800

    test: function return temp

--- test/move_semantics_test.cpp
+++ test/move_semantics_test.cpp
@@ -1,18 +1,28 @@
 #include "gtest/gtest.h"
 
+#define MARK                                                                   \
+  do {                                                                         \
+    std::cout << "[" << __FILE__ << ":" << __LINE__ << "]("                    \
+              << __PRETTY_FUNCTION__ << ")" << std::endl;                      \
+  } while (0)
+
 // class X in namespace x1 with no move semantics
 namespace x1 {
 
 class X {
 public:
-  X() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+  X() { MARK; }
 
-  X(const X &x) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+  X(const X &x) { MARK; }
 
   X &operator=(const X &x) {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    MARK;
     return *this;
   }
+
+private:
+  X(X &&) = delete;
+  X &operator=(X &&) = delete;
 };
 
 } // namespace x1
@@ -23,27 +33,46 @@ namespace x2 {
 class X {
 
 public:
-  X() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+  X() { MARK; }
 
-  X(const X &x) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+  X(const X &x) { MARK; }
 
-  X(X &&x) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
-
+  X(X &&x) { MARK; }
   X &operator=(const X &x) {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    MARK;
     return *this;
   }
 
   X &operator=(X &&x) {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    MARK;
     return *this;
   }
 };
 
 } // namespace x2
 
+namespace x3 {
+x1::X f() {
+  MARK;
+  x1::X x_1{};
+  return x_1;
+}
+
+x2::X g() {
+  MARK;
+  return x2::X();
+}
+
+}; // namespace x3
+
 TEST(move, right_reference) {
   x2::X xx = []() { return x2::X{}; }();
   x2::X x2;
   x2 = x2::X{};
+}
+
+TEST(move, rvo) {
+  // x1::X &x_11 = x3::f(); // need move copy
+  const x1::X &x_1 = x3::f();
+  x2::X x_2 = x3::g();
 }
